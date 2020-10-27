b1804c44f65f361203d84e1688aaa009913a2b60
Wesley <wesley.contato@hotmail.com>
2020-10-27 06:00:11 -0300

    Restringindo o acesso aos endpoints privados

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -24,7 +24,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 
 	// Configurações de recursos estáticos (js, css, imagens, etc)
